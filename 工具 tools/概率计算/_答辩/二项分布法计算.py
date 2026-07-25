b70c8a19575d7dbbbde4090c4745d5310fb61f9c
金合欢酱 <1426540823@qq.com>
2026-07-25 12:55:29 +0800

    Update

--- 工具 tools/概率计算/_答辩/二项分布法计算.py
+++ 工具 tools/概率计算/_答辩/二项分布法计算.py
@@ -2,13 +2,13 @@ import numpy as np
 import scipy
 from scipy.stats import binom
 
-stage = 7
+target_stage = 7
 p = 1/6
 c = 1/4096
 rts = 3
 limit = 10**7
 
-age_values = np.arange(1, stage)
+age_values = np.arange(1, target_stage)
 t = np.arange(1 , limit + 1)
 results = {}
 for age in age_values:
@@ -17,6 +17,6 @@ for age in age_values:
     max = np.argmax(y)
     print(f"当等待时间为{t[max]}t时，可使 age = {age} 的比例取到最大值为 {y[max]}")
     
-h = (1 - binom.cdf(stage - 1 , t * rts, c * p) )   / t
+h = (1 - binom.cdf(target_stage - 1 , t * rts, c * p) )   / t
 max = np.argmax(h)
 print(f"定时收割周期为{t[max]}t时，取到最大的效率为{h[max]}")
